6d992c8d9edf75ed67db049932a8e19704f060d0
Can Madran <can.madran@davon.com.tr>
2017-03-23 22:46:17 +0300

    implemented Printable

--- beeride-server/src/main/java/co/madran/beeride/model/CarpoolUI.java
+++ beeride-server/src/main/java/co/madran/beeride/model/CarpoolUI.java
@@ -1,8 +1,10 @@
 package co.madran.beeride.model;
 
+import co.madran.beeride.http.PrintMedium;
 import co.madran.beeride.model.domain.Carpool;
 
-public class CarpoolUI {
+public class CarpoolUI implements Printable {
+  private Long id;
   private String seats;
   private String plate;
   private String brand;
@@ -10,12 +12,24 @@ public class CarpoolUI {
   private String end;
 
   public CarpoolUI(Carpool carpool) {
+    id = carpool.getId();
     seats = carpool.getCurrentPassengers() + "/"
         + carpool.getCar().getPassengerCount();
     plate = carpool.getCar().getPlate();
     brand = carpool.getCar().getBrand();
     start = carpool.getPath().getStart();
     end = carpool.getPath().getEnd();
+  }
+
+  /** FIXME: this solution is too verbose */
+  @Override
+  public void print(PrintMedium medium) {
+    medium.addProperty("id", id);
+    medium.addProperty("seats", seats);
+    medium.addProperty("plate", plate);
+    medium.addProperty("brand", brand);
+    medium.addProperty("start", start);
+    medium.addProperty("end", end);
   }
 
 }
