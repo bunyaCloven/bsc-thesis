768e6efa480d7cc31d80af6eecdcf18c1e596321
Can Madran <can.madran@davon.com.tr>
2017-03-27 21:59:33 +0300

    CSP-9 Purged unused folders from the repository

--- stouch/stouch/build/temp/production/beeride/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require /home/bunya/DAVON_LIB/WORKSPACES/thesis/beeride-client/app.js
--- stouch/stouch/build/temp/testing/beeride/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require /home/bunya/git/bsc-thesis/beeride-client/app.js
